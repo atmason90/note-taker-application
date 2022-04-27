9d436f7d817a51d8101760ffc13631d5de04fa07
atmason90 <atmason90@gmail.com>
2022-04-26 20:03:59 -0700

    add dependencies and html routes to server.js

---
+++ server.js
@@ -0,0 +1,26 @@
+// Dependencies
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+const db = require('./db/db.json');
+
+const app = express();
+
+const PORT = process.env.PORT || 3001;
+
+// setup Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// give access to all files in public directory
+app.use(express.static('public'));
+
+// html routes
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, 'notes.html'));
+});
+
+// api routes
+
+// Listener
+app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
