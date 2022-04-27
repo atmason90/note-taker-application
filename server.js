6c0d406fb14b572a70c85e360de71c4d9ab6401e
atmason90 <atmason90@gmail.com>
2022-04-26 20:09:03 -0700

    test html routes

--- server.js
+++ server.js
@@ -17,11 +17,11 @@ app.use(express.static('public'));
 
 // html routes
 app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, 'notes.html'));
+    res.sendFile(path.join(__dirname, './public/notes.html'));
 });
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
+    res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
 // api routes
