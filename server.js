eff3432858319cc3425f4f707400d31ab8cbc3cd
atmason90 <atmason90@gmail.com>
2022-04-27 00:10:07 -0700

    add uuid helper

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const fs = require('fs');
 const path = require('path');
 let dbData = require('./db/db.json');
-const { v4: uuidv4 } = require('uuid');
+const uuid = require('./helpers/uuid')
 
 const app = express();
 
