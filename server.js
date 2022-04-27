f71e5a9f59693d716a2fbb894b671fc40695f0da
atmason90 <atmason90@gmail.com>
2022-04-26 20:04:46 -0700

    add html route to server.js

--- server.js
+++ server.js
@@ -20,6 +20,10 @@ app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, 'notes.html'));
 });
 
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
 // api routes
 
 // Listener
